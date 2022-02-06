e983dc8a6005dababc12989108faa5f66151857d
kukipratik <kukipratik123@gmail.com>
2022-02-06 17:40:49 +0545

    all of the glitches fixed

--- lib/widgets/add_button.dart
+++ lib/widgets/add_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterpractice/ItemsData/cart_items.dart';
 import 'package:flutterpractice/ItemsData/items_data.dart';
+import 'package:flutterpractice/Pages/cart_page.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import '../core/store.dart';
@@ -19,7 +20,6 @@ class AddingButton extends StatelessWidget {
     // This expression helps us to use the instance created in store...
     final CartInfo _cart = (VxState.store as MyStore).cart;
     //this expression will let us know if product is already in cart list or not...
-    //made this expression getter sothat there won't be any problem while initializing widget to it....
     bool productAdded =
         (_cart.addedProductsList.contains(product)) ? true : false;
     return ElevatedButton(
@@ -29,9 +29,14 @@ class AddingButton extends StatelessWidget {
       onPressed: () {
         if (!productAdded) {
           AddMutation(product!);
-          debugPrint("No. of item in cart = " +
-              CartInfo().addedProductsList.length.toString());
+          productAdded = !productAdded;
+        } else {
+          RemoveMutation(product!);
+          productAdded = !productAdded;
         }
+      },
+      onLongPress: () {
+        Navigator.push(context,MaterialPageRoute(builder: (context)=> const Cart() ));
       },
       child: productAdded
           ? const Icon(Icons.done)
